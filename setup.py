2557f1189cb30160506bc0a244da67422b55f77c
Don Johnson <dj@codetestcode.io>
2018-10-21 13:27:43 -0700

    added setup file and module folder

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='piipy',
+      version='0.1',
+      description='Utility to find Personally identifiable information',
+      url='https://github.com/copyleftdev/PIIPY',
+      author='Don Johnson',
+      author_email='dj@codetestcode.io',
+      license='MIT',
+      packages=['security'],
+      zip_safe=False)
